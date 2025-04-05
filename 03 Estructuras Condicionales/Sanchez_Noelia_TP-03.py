2d60a5e6a5afe7b157de43f1ae77c9fb8c5227a6
Noelia Sanchez <noelialucianasanchez90@gmail.com>
2025-04-04 21:34:54 -0300

    Resolución Ejercicio 3

--- 03 Estructuras Condicionales/Sanchez_Noelia_TP-03.py
+++ 03 Estructuras Condicionales/Sanchez_Noelia_TP-03.py
@@ -7,6 +7,7 @@ if edad >= 18:
     print("Es mayor de edad")
 
 
+
 # Ejercicio 2: Escribir un programa que solicite su nota al usuario. Si la nota es mayor o igual a 6, deberá mostrar por 
 # pantalla un mensaje que diga “Aprobado”; en caso contrario deberá mostrar el mensaje “Desaprobado”.
 
@@ -17,3 +18,18 @@ if nota >= 6:
 # En cualquier otro caso, imprimir "Desaprobado""
 else:
     print("Desaprobado")
+
+
+
+# Ejercicio 3: Escribir un programa que permita ingresar solo números pares. Si el usuario ingresa un número par, 
+# imprimir por en pantalla el mensaje "Ha ingresado un número par"; en caso contrario, imprimir por pantalla 
+# "Por favor, ingrese un número par". Nota: investigar el uso del operador de módulo (%) en Python para evaluar 
+# si un número es par o impar.
+
+num_par=int(input("Por favor, ingrese un número par: "))
+# Si al dividir num_par por 2 el resto es igaul a 0, imprimir "Ha ingresado un número par"
+if num_par % 2 == 0:
+    print("Ha ingresado un número par")
+# Si no, si el resto es distinto de cero, imprimir "Por favor, ingrese un número par"
+else:
+    print("Por favor, ingrese un número par")
