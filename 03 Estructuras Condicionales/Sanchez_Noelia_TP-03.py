2df545803769b6bda1e987b6f96b4c7c45080f71
Noelia Sanchez <noelialucianasanchez90@gmail.com>
2025-04-04 21:15:56 -0300

    Resolución Ejercicio 2

--- 03 Estructuras Condicionales/Sanchez_Noelia_TP-03.py
+++ 03 Estructuras Condicionales/Sanchez_Noelia_TP-03.py
@@ -2,5 +2,18 @@
 #mostrar un mensaje en pantalla que diga “Es mayor de edad”.
 
 edad=int(input("Por favor, ingrese su edad: "))
+# Si edad es mayor o igal que 18, imprimir "Es mayor de edad"
 if edad >= 18:
     print("Es mayor de edad")
+
+
+# Ejercicio 2: Escribir un programa que solicite su nota al usuario. Si la nota es mayor o igual a 6, deberá mostrar por 
+# pantalla un mensaje que diga “Aprobado”; en caso contrario deberá mostrar el mensaje “Desaprobado”.
+
+nota=int(input("Ingrese su nota: "))
+# Si la nota ingresada es mayor que 6, imprimir "Aprobado"
+if nota >= 6:
+    print("Aprobado")
+# En cualquier otro caso, imprimir "Desaprobado""
+else:
+    print("Desaprobado")
