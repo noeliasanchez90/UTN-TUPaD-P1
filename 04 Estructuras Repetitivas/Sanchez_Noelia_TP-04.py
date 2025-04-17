95db78fc36fb223e286a995b9ffe1042c1862ed7
Noelia Sanchez <noelialucianasanchez90@gmail.com>
2025-04-17 14:27:40 -0300

    Resolución Ejercicio 2

--- 04 Estructuras Repetitivas/Sanchez_Noelia_TP-04.py
+++ 04 Estructuras Repetitivas/Sanchez_Noelia_TP-04.py
@@ -4,3 +4,14 @@
 for cont in range (101):
     print (cont)
    # Ciclo for que muestra los números desde 0 hasta 100
+
+
+
+# Ejercicio 2: Desarrolla un programa que solicite al usuario un número entero y determine la cantidad de
+# dígitos que contiene.
+
+num = int(input("Ingrese un número entero: "))
+num_entero = abs(num)
+dígitos = len(str(num_entero))
+print("El número ingresado,", num,", tiene", dígitos, "dígitos")
+
