ceb6e57b9a6812e23b56ab31f7a7163ba8de3d48
Noelia Sanchez <noelialucianasanchez90@gmail.com>
2025-04-17 14:58:03 -0300

    Resolución Ejercicio 3

--- 04 Estructuras Repetitivas/Sanchez_Noelia_TP-04.py
+++ 04 Estructuras Repetitivas/Sanchez_Noelia_TP-04.py
@@ -10,8 +10,33 @@ for cont in range (101):
 # Ejercicio 2: Desarrolla un programa que solicite al usuario un número entero y determine la cantidad de
 # dígitos que contiene.
 
+#Se solicita ingresar un número entero al usuario.
 num = int(input("Ingrese un número entero: "))
+#Le aplicamos el valor absoluto para contar dígitos sin el singno "-" si es negativo.
 num_entero = abs(num)
+#Aplicamos la función len() para contar dígitos
 dígitos = len(str(num_entero))
 print("El número ingresado,", num,", tiene", dígitos, "dígitos")
 
+
+
+# Ejercicio 3: Escribe un programa que sume todos los números enteros comprendidos entre dos valores
+# dados por el usuario, excluyendo esos dos valores.
+
+# Solicito ingresar los números enteros al usuario:
+print("Este programa devolverá la suma de los números comprendidos entre dos valores ingresados por el usuario:")
+num_inicial = int(input("Ingrese el primer núm. entero: "))
+num_final = int(input("Ingrese el segundo núm. entero: "))
+# Inicializo la variable sumatoria en 0 
+sumatoria = 0
+
+# Evalúo con la función if cual de los dos número ingresados es mayor para poder sumar los núm del medio en forma creciente
+if num_final > num_inicial:
+    for num in range(num_inicial + 1, num_final):
+        sumatoria += num
+else:
+    for num in range(num_final + 1, num_inicial):
+        sumatoria += num
+
+# Muestro por pantalla el valor de la sumatoria
+print("La sumatoria de los num comprendidos entre ", num_inicial, "y", num_final, "es:", sumatoria)
