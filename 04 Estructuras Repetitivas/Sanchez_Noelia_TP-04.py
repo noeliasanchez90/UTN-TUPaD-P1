d91e049c546862795d103f009a39ad9c222a45de
Noelia Sanchez <noelialucianasanchez90@gmail.com>
2025-04-17 23:40:17 -0300

    Resolución Ejercicio 5

--- 04 Estructuras Repetitivas/Sanchez_Noelia_TP-04.py
+++ 04 Estructuras Repetitivas/Sanchez_Noelia_TP-04.py
@@ -46,11 +46,36 @@ print("La sumatoria de los num comprendidos entre ", num_inicial, "y", num_final
 # Ejercicio 4: Elabora un programa que permita al usuario ingresar números enteros y los sume en secuencia. El programa 
 # debe detenerse y mostrar el total acumulado cuando el usuario ingrese un 0.
 
-num = int(input("ingrese un número entero: "))
+num = int(input("ingrese un número entero (0 para salir): "))
 sumatoria = 0
 
-while num != 0:
+# Mientras los números ingresados sean distinto de 0, se irán sumando secuencialmente acumulado.
+while num != 0: 
     sumatoria = sumatoria + num
-    num = int(input("Ingrese otro número entero: "))
+    num = int(input("Ingrese otro número entero (0 para salir): "))
 
+# Mostrar el total acumulado de los números ingresados
 print("La sumatoria de los números ingresados es = ", sumatoria)
+
+
+
+# Ejercicio 5: Crea un juego en el que el usuario deba adivinar un número aleatorio entre 0 y 9. Al final, el
+# programa debe mostrar cuántos intentos fueron necesarios para acertar el número.
+
+# Se importa la librería random
+import random
+#Se le solicita al usuario que adivine un núm entre 0 y 9
+num = int(input("Adivine el número entre 0 y 9:"))
+# Se aplica la función randint() para generar un num aleatorio entre 0 y 9
+num_aleatorio = random.randint(0, 9)
+print(num_aleatorio)
+# Se inicializa un contador para contar la cantidad de intentos hasta adivinar el número.
+cont = 1
+
+# Inicializamos el bucle while hasta que el número ingresado por el usuario sea igual al aleatorio, ahí corta
+while num != num_aleatorio:
+    cont += 1
+    num = int(input("Adivine el número entre 0 y 9:"))
+
+# Se muestra por pantalla la cantidad de intentos.
+print("se hicieron ", cont, "intentos")
