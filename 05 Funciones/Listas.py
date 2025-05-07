6814cf8e201baf9b1a474f323a9263e9f8107361
Noelia Sánchez <noelialucianasanchez90@gmail.com>
2025-05-06 22:16:35 -0300

    Resolución ejercicio 1

---
+++ 05 Funciones/Listas.py
@@ -0,0 +1,11 @@
+# 1) Crear una lista con los números del 1 al 100 que sean múltiplos de 4. Utilizar la función range. 
+
+# Se utiliza la función list() para crear una lista de números y por dentro la función range() para definir el rango y los saltos de números
+lista_multiplo_4 = list(range(1,101,4))  
+ 
+# Imprimo por pantalla la lista generada en la variable lista_multiplo_4
+print(lista_multiplo_4)
+
+
+
+
