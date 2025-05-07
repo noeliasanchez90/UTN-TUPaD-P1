f89b43c509a0d70114de44ccabe2ac00993d4b1e
Noelia Sánchez <noelialucianasanchez90@gmail.com>
2025-05-06 23:11:42 -0300

    Resolución ejercicio 2

--- 05 Funciones/Listas.py
+++ 05 Funciones/Listas.py
@@ -9,3 +9,10 @@ print(lista_multiplo_4)
 
 
 
+# 2) Crear una lista con cinco elementos (colocar los elementos que más te gusten) y mostrar el penúltimo. ¡Puedes hacerlo como 
+# se muestra en los videos o bien investigar cómo funciona el indexing con números negativos!
+ 
+# Se crea una lista cuyos elementos son frutas
+lista_elementos = ['manzana', 'banana', 'pera', 'kiwy', 'ciruela']
+print(lista_elementos[3]) # Muestra el elemento de la posición 2 de la lista, contabilizando desde el 0 que corresponde a 'manzana'
+print(lista_elementos[-2]) # indexing con número negativo, contabiliza desde el último elemento hacia el primero empezando del -1
