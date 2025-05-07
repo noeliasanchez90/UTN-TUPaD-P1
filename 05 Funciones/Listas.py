9fbe5099c1e153fc0b9642bd89cd6abbb75c74f5
Noelia Sánchez <noelialucianasanchez90@gmail.com>
2025-05-07 20:49:20 -0300

    Resolución ejercicio 9

--- 05 Funciones/Listas.py
+++ 05 Funciones/Listas.py
@@ -89,3 +89,26 @@ dobles.append(10 * 2)
 dobles.append(15 * 2)
 
 print(dobles)   # Se imprime por pantalla la lista resultante
+
+
+
+
+# 9) Dada la lista “compras”, cuyos elementos representan los productos comprados por diferentes clientes: 
+
+compras = [["pan", "leche"], ["arroz", "fideos", "salsa"], ["agua"]] 
+
+# a) Agregar "jugo" a la lista del tercer cliente usando append.
+
+compras[2].append("jugo")
+
+# b) Reemplazar "fideos" por "tallarines" en la lista del segundo cliente. 
+
+compras[1][1] = "tallarines"
+
+# c) Eliminar "pan" de la lista del primer cliente.  
+
+compras[0].remove("pan")
+
+# d) Imprimir la lista resultante por pantalla
+
+print(compras)
