39f4483116d13ee6bd713c540223b2ffcdd2c0f4
Noelia Sánchez <noelialucianasanchez90@gmail.com>
2025-05-07 21:05:14 -0300

    Resolución ejercicio 10

--- 05 Funciones/Listas.py
+++ 05 Funciones/Listas.py
@@ -112,3 +112,19 @@ compras[0].remove("pan")
 # d) Imprimir la lista resultante por pantalla
 
 print(compras)
+
+
+
+
+# 10) Elaborar una lista anidada llamada “lista_anidada” que contenga los siguientes elementos: 
+# ● Posición lista_anidada[0]: 15 
+# ● Posición lista_anidada[1]: True 
+# ● Posición lista_anidada[2][0]: 25.5 
+# ● Posición lista_anidada[2][1]: 57.9 
+# ● Posición lista_anidada[2][2]: 30.6 
+# ● Posición lista_anidada[3]: False 
+# Imprimir la lista resultante por pantalla. 
+
+lista_anidada = [15, True, [25.5, 57.9, 30.6], [False]] # Lista creada según consigna
+
+print(lista_anidada) # Se imprime la lista resultante
