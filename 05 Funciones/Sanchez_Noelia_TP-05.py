4cbf70b69668526034a82f5c7ce115a1149019a7
Noelia Sanchez <noelialucianasanchez90@gmail.com>
2025-05-24 14:10:29 -0300

    Resolución Ejercicio 2

--- 05 Funciones/Sanchez_Noelia_TP-05.py
+++ 05 Funciones/Sanchez_Noelia_TP-05.py
@@ -7,3 +7,18 @@ def imprimir_hola_mundo():
 
 # Programa principal
 imprimir_hola_mundo()
+
+
+
+
+# Ejercicio 2: Crear una función llamada saludar_usuario(nombre) que reciba como parámetro un nombre y devuelva un 
+# saludo personalizado. Por ejemplo, si se llama con saludar_usuario("Marcos"), deberá devolver: “Hola Marcos!”.
+# Llamar a esta función desde el programa principal solicitando el nombre al usuario.
+
+# Definición de funciones
+def saludar_usuario(nombre):
+    return print(f"Hola {nombre}!")
+
+# Programa principal
+nombre = input("Ingrese su nombre: ")
+saludar_usuario(nombre)
