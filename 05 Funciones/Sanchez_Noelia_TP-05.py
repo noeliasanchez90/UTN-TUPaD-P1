c9cecfb3151a6acb97acfd5b183afb0a5f2bcfc6
Noelia Sanchez <noelialucianasanchez90@gmail.com>
2025-05-24 13:33:35 -0300

    Resolución Ejercicio 1

---
+++ 05 Funciones/Sanchez_Noelia_TP-05.py
@@ -0,0 +1,9 @@
+# Ejercicio 1: 1. Crear una función llamada imprimir_hola_mundo que imprima porpantalla el mensaje:
+# “Hola Mundo!”. Llamar a esta función desde el programa principal.
+
+# Definición de funciones
+def imprimir_hola_mundo():
+    return print("Hola Mundo!")
+
+# Programa principal
+imprimir_hola_mundo()
