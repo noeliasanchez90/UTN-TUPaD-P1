90b087cedb4673d9f8e5f23a1e844af84f2b103f
Noelia Sanchez <noelialucianasanchez90@gmail.com>
2025-05-25 20:19:32 -0300

    Resolución Ejercicio 2

--- 11 Recursividad/Sanchez_Noelia_TP-07.py
+++ 11 Recursividad/Sanchez_Noelia_TP-07.py
@@ -2,23 +2,40 @@
 # calcular y mostrar en pantalla el factorial de todos los números enteros entre 1 y el número que indique el usuario
 
 # Función recursiva para calcular factorial 
-def factorial(num):
-    if num == 0:
-        return 1
-    else:
-        return num * factorial(num - 1)
+#def factorial(num):
+#    if num == 0:
+#        return 1
+#    else:
+#        return num * factorial(num - 1)
 
 # Pedir al usuario que ingrese un num para calcular el factorial:
-num = int(input("ingrese un número entero positivo: "))
+#num = int(input("ingrese un número entero positivo: "))
 
-if num < 0:
-    num = int(print("Por favor, ingrese un número entero positivo: "))
-else:
-    print(f"Factoriales del 1 al {num}:")
-    for i in range(1, num + 1):
-        print(f"{i}! = {factorial(i)}")
-
-
+#if num < 0:
+#    num = int(print("Por favor, ingrese un número entero positivo: "))
+#else:
+#    print(f"Factoriales del 1 al {num}:")
+#    for i in range(1, num + 1):
+#        print(f"{i}! = {factorial(i)}")
 
 
-# Ejercicio 2: 
+
+
+# Ejercicio 2: Crea una función recursiva que calcule el valor de la serie de Fibonacci en la posición indicada.
+# Posteriormente, muestra la serie completa hasta la posición que el usuario especifique.
+
+# definir función Fibonacci
+def fibonacci(pos):
+
+    if pos == 0:
+        return 0
+    elif pos == 1:
+        return 1
+    else:
+        return fibonacci(pos-1) + fibonacci(pos-2)
+
+# solicitar al usuario que especifique la posición en la serie de Fibonacci 
+pos = int(input("Ingrese la posición de la serie de Fibonacci hasta la que quieres ver: "))
+
+for i in range(0, pos + 1):
+    print(f"F({i}) = {fibonacci(i)}")
