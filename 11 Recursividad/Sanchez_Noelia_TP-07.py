f3826c0ec6ceae1d523a7891aaf608133dd3d970
Noelia Sanchez <noelialucianasanchez90@gmail.com>
2025-05-25 23:39:28 -0300

    Creación README TP Recursividad

--- 11 Recursividad/Sanchez_Noelia_TP-07.py
+++ 11 Recursividad/Sanchez_Noelia_TP-07.py
@@ -2,21 +2,21 @@
 # calcular y mostrar en pantalla el factorial de todos los números enteros entre 1 y el número que indique el usuario
 
 # Función recursiva para calcular factorial 
-#def factorial(num):
-#    if num == 0:
-#        return 1
-#    else:
-#        return num * factorial(num - 1)
+def factorial(num):
+    if num == 0:
+        return 1
+    else:
+        return num * factorial(num - 1)
 
 # Pedir al usuario que ingrese un num para calcular el factorial:
-#num = int(input("ingrese un número entero positivo: "))
+num = int(input("ingrese un número entero positivo: "))
 
-#if num < 0:
-#    num = int(print("Por favor, ingrese un número entero positivo: "))
-#else:
-#    print(f"Factoriales del 1 al {num}:")
-#    for i in range(1, num + 1):
-#        print(f"{i}! = {factorial(i)}")
+if num < 0:
+    num = int(print("Por favor, ingrese un número entero positivo: "))
+else:
+    print(f"Factoriales del 1 al {num}:")
+    for i in range(1, num + 1):
+        print(f"{i}! = {factorial(i)}")
 
 
 
@@ -25,20 +25,20 @@
 # Posteriormente, muestra la serie completa hasta la posición que el usuario especifique.
 
 # definir función Fibonacci
-#def fibonacci(pos):
+def fibonacci(pos):
 
-#    if pos == 0:
-#        return 0
-#    elif pos == 1:
-#        return 1
-#    else:
-#        return fibonacci(pos-1) + fibonacci(pos-2)
+    if pos == 0:
+        return 0
+    elif pos == 1:
+        return 1
+    else:
+        return fibonacci(pos-1) + fibonacci(pos-2)
 
 # solicitar al usuario que especifique la posición en la serie de Fibonacci 
-#pos = int(input("Ingrese la posición de la serie de Fibonacci hasta la que quieres ver: "))
+pos = int(input("Ingrese la posición de la serie de Fibonacci hasta la que quieres ver: "))
 
-#for i in range(0, pos + 1):
-#    print(f"F({i}) = {fibonacci(i)}")
+for i in range(0, pos + 1):
+    print(f"F({i}) = {fibonacci(i)}")
 
 
 
@@ -46,18 +46,18 @@
 # Ejercicio 3: Crea una función recursiva que calcule la potencia de un número base elevado a un exponente, utilizando
 # la fórmula 𝑛𝑚= 𝑛∗𝑛(𝑚−1). Prueba esta función en un algoritmo general.
 
-#def potencia(n, m):
-#    if m == 0:
-#        return 1 # Cualquier número elevado a la 0 es 1
-#    else:
+def potencia(n, m):
+    if m == 0:
+        return 1 # Cualquier número elevado a la 0 es 1
+    else:
 #        return n * potencia(n, m-1)
 
 # Algoritmo general para probar la función
-#base = int(input("Introduce la base (n): "))
-#exponente = int(input("Introduce el exponente (m): "))
+base = int(input("Introduce la base (n): "))
+exponente = int(input("Introduce el exponente (m): "))
 
-#resultado = potencia(base, exponente)
-#print(f"{base} elevado a la {exponente} es: {resultado}")
+resultado = potencia(base, exponente)
+print(f"{base} elevado a la {exponente} es: {resultado}")
 
 
 
@@ -136,7 +136,7 @@ print(f"La cantidad de bloques ingresado es: {contar_bloques(4)}")
 
 def contar_digito(numero, digito):
    if numero == 0:
-     return 0
+     return 0   
    else:
     ultimo = numero % 10
     resto = numero // 10
