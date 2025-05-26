eeb03fc7d238af782f3ba05d6f63f29aa0fc7116
Noelia Sanchez <noelialucianasanchez90@gmail.com>
2025-05-25 22:33:08 -0300

    Resolución Ejercicio 6

--- 11 Recursividad/Sanchez_Noelia_TP-07.py
+++ 11 Recursividad/Sanchez_Noelia_TP-07.py
@@ -96,3 +96,17 @@ def es_palindromo(texto):
   
 print(f"NOELIA es palidromo: {es_palindromo("NOELIA")}")
 print(f"ANA es palidromo: {es_palindromo("ANA")}")
+
+
+
+
+# Ejercicio 6: Escribí una función recursiva en Python llamada suma_digitos(n) que reciba un número entero positivo y 
+# devuelva la suma de todos sus dígitos.
+
+def suma_digitos(n):
+  if n < 10:
+    return n
+  else:
+    return n % 10 + suma_digitos(n // 10)
+  
+print(f"La suma de los dígitos  es: {suma_digitos(2523)}")
