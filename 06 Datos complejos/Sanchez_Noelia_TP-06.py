3f943d5d4a026becfdbfb4ada28ed29b782ce12f
Noelia Sanchez <noelialucianasanchez90@gmail.com>
2025-06-03 19:11:18 -0300

    Resolución Ejercicio 1

---
+++ 06 Datos complejos/Sanchez_Noelia_TP-06.py
@@ -0,0 +1,15 @@
+# Ejercicio 1: Dado el diccionario precios_frutas precios_frutas = {'Banana': 1200, 'Ananá': 2500, 
+# 'Melón': 3000, 'Uva': 1450} Añadir las siguientes frutas con sus respectivos precios:
+# ● Naranja = 1200
+# ● Manzana = 1500
+# ● Pera = 2300
+
+precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva': 1450}
+
+# Se añaden nuevas frutas
+precios_frutas['Naranja'] = 1200
+precios_frutas['Manzana'] = 1500
+precios_frutas['Pera'] = 2300
+
+# Mostrar resultado
+print(precios_frutas)
